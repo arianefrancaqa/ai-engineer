6501c65e83fb460b7c8fa13e1e86138e7059e35a
arianefrancaqa <arianefrancaqa@gmail.com>
2025-01-29 16:04:43 -0300

    adding initial structure

---
+++ index.js
@@ -0,0 +1,23 @@
+import OpenAI from "openai"
+
+const openai = new OpenAI({
+    dangerouslyAllowBrowser: true
+})
+
+const messages = [
+    {
+        role: 'system',
+        content: 'You are a helpful general knowledge expert.'
+    },
+    {
+        role: 'user',
+        content: 'Who invented the television?'
+    }
+]
+
+const response = await openai.chat.completions.create({
+    model: 'gpt-4',
+    messages: messages
+})
+
+console.log(response.choices[0].message.content)
